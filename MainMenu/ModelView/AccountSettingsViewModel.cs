21c30f977ee755600c76a5c729025a3def8aad6c
RasmusChristensen <rasmus@christensen.io>
2018-10-10 13:42:11 +0200

    Rewroked accountsettings layout. Redid AccountSettingsView.xaml.cs

--- MainMenu/ModelView/AccountSettingsViewModel.cs
+++ MainMenu/ModelView/AccountSettingsViewModel.cs
@@ -1,25 +1,15 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using MainMenu.ModelView;
 
 
-namespace MainMenu
+namespace MainMenu.ModelView
 {
-    /// <summary>
-    /// Interaction logic for AccountSettingsView.xaml
-    /// </summary>
-    public partial class AccountSettingsViewModel : UserControl
+    public class AccountSettingsView : ViewModelCore
     {
 
 
-        public AccountSettingsViewModel()
-        {
-            InitializeComponent();
-        }
-
-        //DER MAGLER CHECKS FOR OM USERNAME OG PASSWORD ER DET SAMME SOM DER ALLEREDE STÅR I DB. DERUDOVER MANGLER VI OGSÅ EN 
-        //FUNCTION DER SLØRRE SAVE BTN INDTIL DER ER INDTASTET NYT DATA I ENTEN USERNAME CHANGE ELLER PASSWORD CHANGE
-        
-
     }
+    
 }
