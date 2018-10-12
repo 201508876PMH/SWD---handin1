2ca96dbb4362bc0b64b287d6c698377a474da5ef
Peter Marcus Hoveling <au536878@uni.au.dk>
2018-10-12 12:55:47 +0200

    added funktionality

---
+++ MainMenu/Interfaces/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MainMenu.Interfaces
+{
+    interface INavigationService
+    {
+        // The 'showView' method take a UserControl object type argument, reason our views implement UserControl
+        void showView(UserControl view);
+    }
+}
